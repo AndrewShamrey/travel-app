dfabadc44f6345cef42e88b66dea06294ca7bedf
Yuliya Reutovich <yrevtovich@mail.ru>
2021-03-13 02:20:40 +0300

    country-page/feat: integrate store data, add changin language

--- src/components/countryPage/countryPage.jsx
+++ src/components/countryPage/countryPage.jsx
@@ -1,59 +1,60 @@
+import { useSelector } from 'react-redux';
+import dateWidget from '../../assets/images/date-widget.png';
+import weatherWidget from '../../assets/images/weather-widget.png';
+import exchangeRatesWidget from '../../assets/images/exchange-rates-widget.png';
+import mapImg from '../../assets/images/map.png';
 import './countryPage.scss';
 
 const CountryPage = () => {
-  console.log('countryData');
+  const countryData = useSelector((rootState) => rootState.control.countryConfig);
+  const currentLanguage = useSelector((rootState) => rootState.control.applicationLanguage);
+
+  const { name, capital, description } = countryData.info[currentLanguage];
+  const { mainPlace } = countryData;
 
   return (
     <main className="country-page">
       <div className="country-page__wrapper">
         <div className="country-page__info">
           <div className="country-page__country">
-            <h2 className="country-page__name">Australia</h2>
-            <p className="country-page__capital">Canberra</p>
+            <h2 className="country-page__name">{name}</h2>
+            <p className="country-page__capital">{capital}</p>
           </div>
           <img
             className="country-page__image"
-            src="assets/images/australia-img-example.jpg"
-            alt="country"
+            src={mainPlace.image}
+            alt={mainPlace[currentLanguage].name}
           />
-          <p className="country-page__description">
-            Australia, officially the Commonwealth of Australia, is a sovereign country comprising
-            the mainland of the Australian continent, the island of Tasmania, and numerous smaller
-            islands. It is the largest country in Oceania and the world&lsquo;s sixth-largest
-            country by total area. Its population of nearly 26 million is highly urbanised and
-            heavily concentrated on the eastern seaboard. Australia&lsquo;s capital is Canberra,
-            and its largest city is Sydney. The country&lsquo;s other major metropolitan areas are
-            Melbourne, Brisbane, Perth, and Adelaide.
-          </p>
+          <p className="country-page__description">{description}</p>
           <div className="country-page__widgets">
             <div className="country-page__widget">
-              <img src="assets/images/date-widget.png" alt="Date widget" />
+              <img src={dateWidget} alt="Date widget" />
             </div>
             <div className="country-page__widget">
-              <img src="assets/images/weather-widget.png" alt="Weather widget" />
+              <img src={weatherWidget} alt="Weather widget" />
             </div>
             <div className="country-page__widget">
-              <img src="assets/images/exchange-rates-widget.png" alt="Exchange rates widget" />
+              <img src={exchangeRatesWidget} alt="Exchange rates widget" />
             </div>
           </div>
           <div className="country-page__gallery">
             <img
               className="country-page__image"
-              src="assets/images/australia-img-example.jpg"
+              src={mainPlace.image}
               alt="example"
             />
           </div>
           <div className="country-page__video">
             <img
               className="country-page__image"
-              src="assets/images/australia-img-example.jpg"
+              src={mainPlace.image}
               alt="example"
             />
           </div>
         </div>
       </div>
       <div className="country-page__map">
-        <img src="assets/images/map.png" alt="map example" />
+        <img src={mapImg} alt="map example" />
       </div>
     </main>
   );
