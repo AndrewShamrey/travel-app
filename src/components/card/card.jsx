6040b8e76f76d73ea95d66aad675f4269bcb62aa
angoulema <eugenia.labokha@yandex.by>
2021-03-14 13:39:27 +0300

    routing / fix: remove unused vars

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,21 +1,13 @@
-import { useDispatch } from 'react-redux';
-import FetchData from '../../utils/fetchData';
 import { MAIN_PLACES } from '../../utils/constants';
 import './card.css';
 
-const Card = ({ item, index }) => {
-  const fetchClass = new FetchData('https://travel-app-back-113.herokuapp.com/api');
-
-  const dispatch = useDispatch();
-
-  return (
-    <div key={item} country={item} className="country-card" role="presentation">
-      <img src={MAIN_PLACES[index]} alt="place" className="country-card__image" />
-      <div className="country-card__title">
-        {item}
-      </div>
+const Card = ({ item, index }) => (
+  <div key={item} country={item} className="country-card" role="presentation">
+    <img src={MAIN_PLACES[index]} alt="place" className="country-card__image" />
+    <div className="country-card__title">
+      {item}
     </div>
-  );
-};
+  </div>
+);
 
 export default Card;
