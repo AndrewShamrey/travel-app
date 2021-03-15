587995d0397d8164550f08a9d647feb7dbd525b6
Yuliya Reutovich <yrevtovich@mail.ru>
2021-03-15 14:27:22 +0300

    search/feat: animate search request

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,24 +1,14 @@
-import { current } from 'immer';
 import { useRef, useEffect } from 'react';
 import { MAIN_PLACES } from '../../utils/constants';
 import './card.scss';
 
-const Card = ({ country, capital, index }) => {
-  const ref = useRef();
-  console.log(ref?.current?.selectionStart, ref?.current?.selectionEnd);
-  // useEffect(() => {
-  //   ref.current.getSelection().setRange(1, 2);
-  //   // ref.current.selectionEnd = 10;
-  // }, [ref]);
-
-  return (
-    <div className="country-card" role="presentation">
-      <img src={MAIN_PLACES[index]} alt="place" className="country-card__image" />
-      <p className="country-card__title" ref={ref}>
-        {`${country} - ${capital}`}
-      </p>
-    </div>
-  );
-};
+const Card = ({ country, capital, index }) => (
+  <div className="country-card" role="presentation">
+    <img src={MAIN_PLACES[index]} alt="place" className="country-card__image" />
+    <p className="country-card__title">
+      {`${country} - ${capital}`}
+    </p>
+  </div>
+);
 
 export default Card;
