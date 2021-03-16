a2e093ae10aa1cace5e30ca49d20dc44b1598b6d
Yuliya Reutovich <yrevtovich@mail.ru>
2021-03-17 01:17:34 +0300

    exchange-rates-widget/feat: add reload exchange rates, spinner

--- src/components/echangeRateWidget/exchangeRateWidget.jsx
+++ src/components/echangeRateWidget/exchangeRateWidget.jsx
@@ -1,46 +1,80 @@
 import { useState, useEffect, useCallback } from 'react';
 import { useSelector } from 'react-redux';
-import { exchangeRatesAPI } from '../../utils/exchangeAPI';
+import exchangeRatesAPI from '../../utils/exchangeAPI';
 import './exchangeRatesWidget.scss';
 
 const ExchangeRateWidget = () => {
   const currency = useSelector((rootState) => rootState.control.countryConfig.currency.code);
   const [rates, setRates] = useState({});
+  const [isLoader, setIsLoader] = useState(true);
+  const [isError, setIsError] = useState(false);
 
   const getExchangeRates = useCallback(async () => {
     try {
+      setIsLoader(() => true);
       const { EUR, USD, BYN } = await exchangeRatesAPI.get(currency);
-      setRates({ EUR, USD, BYN });
+      setRates({
+        EUR: EUR.toFixed(5),
+        USD: USD.toFixed(5),
+        BYN: BYN.toFixed(5),
+      });
+
+      setIsLoader(() => false);
     } catch (e) {
-      console.log(e);
+      setIsLoader(() => false);
+      setIsError(() => true);
     }
   }, [currency]);
+
+  const handleOnClick = () => {
+    setIsError(() => false);
+    getExchangeRates();
+  };
 
   useEffect(() => {
     getExchangeRates();
   }, [getExchangeRates]);
-
-  console.log(rates);
 
   return (
     <div className="exchange-rates-widget">
       <p className="exchange-rates-widget__title">
         {`${currency} exchange rates`}
       </p>
-      <ul className="exchange-rates-widget__list">
-        <li className="exchange-rates-widget__list-item">
-          <span className="exchange-rates-widget__currency">EUR</span>
-          <span className="exchange-rates-widget__value">{rates.EUR || 'Not found'}</span>
-        </li>
-        <li className="exchange-rates-widget__list-item">
-          <span className="exchange-rates-widget__currency">USD</span>
-          <span className="exchange-rates-widget__value">{rates.USD || 'Not found'}</span>
-        </li>
-        <li className="exchange-rates-widget__list-item">
-          <span className="exchange-rates-widget__currency">BYN</span>
-          <span className="exchange-rates-widget__value">{rates.BYN || 'Not found'}</span>
-        </li>
-      </ul>
+      <div className="exchange-rates-widget__list">
+        {isLoader && (
+          <span className="exchange-rates-widget__spinner">
+            <i className="fas fa-spinner" />
+          </span>
+        )}
+        {isError && (
+          <>
+            <button
+              className="exchange-rates-widget__reload-btn"
+              type="button"
+              onClick={handleOnClick}
+            >
+              <i className="fas fa-sync-alt" />
+            </button>
+            <p>Server is not available</p>
+          </>
+        )}
+        {!isError && !isLoader && (
+          <ul>
+            <li className="exchange-rates-widget__list-item">
+              <span className="exchange-rates-widget__currency">EUR</span>
+              <span className="exchange-rates-widget__value">{rates.EUR || 'Not found'}</span>
+            </li>
+            <li className="exchange-rates-widget__list-item">
+              <span className="exchange-rates-widget__currency">USD</span>
+              <span className="exchange-rates-widget__value">{rates.USD || 'Not found'}</span>
+            </li>
+            <li className="exchange-rates-widget__list-item">
+              <span className="exchange-rates-widget__currency">BYN</span>
+              <span className="exchange-rates-widget__value">{rates.BYN || 'Not found'}</span>
+            </li>
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
--- src/utils/exchangeAPI.js
+++ src/utils/exchangeAPI.js
@@ -1,7 +1,4 @@
-export const EXCHANGE_RATES_API_KEY = 'e114446e8cf339d37f9468a0';
-export const EXCHANGE_RATES_API_URL = 'https://v6.exchangerate-api.com/v6/';
-
-export const exchangeRatesAPI = {
+const exchangeRatesAPI = {
   url: 'https://v6.exchangerate-api.com/v6/',
   key: 'e114446e8cf339d37f9468a0',
   async get(currency) {
@@ -15,8 +12,10 @@ export const exchangeRatesAPI = {
       const exchangeRates = await response.json();
 
       return exchangeRates.conversion_rates;
-    } catch (e) {
-      return e;
+    } catch (error) {
+      return error;
     }
   },
 };
+
+export default exchangeRatesAPI;
